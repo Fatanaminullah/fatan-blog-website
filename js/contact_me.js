42e9588080fb02eb515e3275a2e6b410b7c8e369
Fatan <fatan.aminullah.j@gmail.com>
2021-06-04 10:36:03 +0700

    update submit form

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,85 +1,62 @@
-$(function () {
-  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-    preventSubmit: true,
-    submitError: function ($form, event, errors) {
-      // additional error messages or events
-    },
-    submitSuccess: function ($form, event) {
-      event.preventDefault(); // prevent default submit behaviour
-      // get values from FORM
-      var from_name = $("input#name").val();
-      var reply_to = $("input#email").val();
-      var subject = $("input#subject").val();
-      var message = $("textarea#message").val();
-      var firstName = from_name; // For Success/Failure Message
-      // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(" ") >= 0) {
-        firstName = name.split(" ").slice(0, -1).join(" ");
+(function () {
+  let form = document.getElementById("contactForm");
+  let emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let stringRegex = /^([^0-9]*)$/;
+  form.addEventListener("submit", (event) => {
+    // ...
+    // stop form submission
+    event.preventDefault();
+    let from_name = form.elements["name"].value;
+    let reply_to = form.elements["email"].value;
+    let subject = form.elements["subject"].value;
+    let message = form.elements["message"].value;
+    let nameError, emailError;
+    if (!from_name || !reply_to || !subject || !message) {
+      alert("Field cannot be empty!");
+    } else {
+      if (!emailRegex.test(reply_to)) {
+        emailError = "Wrong email format!";
       }
-      $this = $("#sendMessageButton");
-      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-      emailjs
-        .send("service_gmail", "template_default", {
-          from_name,
-          subject,
-          reply_to,
-          message,
-          to_name: "Fatan",
-        })
-        .then(
-          function () {
-            // Success message
-            $("#success").html("<div class='alert alert-success'>");
-            $("#success > .alert-success")
-              .html(
-                "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-              )
-              .append("</button>");
-            $("#success > .alert-success").append(
-              "<strong>Your message has been sent. </strong>"
-            );
-            $("#success > .alert-success").append("</div>");
-            //clear all fields
-            $("#contactForm").trigger("reset");
-          },
-          function (error) {
-            // Fail message
-            $("#success").html("<div class='alert alert-danger'>");
-            $("#success > .alert-danger")
-              .html(
-                "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-              )
-              .append("</button>");
-            $("#success > .alert-danger").append(
-              $("<strong>").text(
-                "Sorry " +
-                  firstName +
-                  ", it seems that my mail server is not responding. Please try again later!"
-              )
-            );
-            $("#success > .alert-danger").append("</div>");
-            //clear all fields
-            $("#contactForm").trigger("reset");
-          }
-        )
-        .finally(function () {
-          setTimeout(function () {
-            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-          }, 1000);
-        });
-    },
-    filter: function () {
-      return $(this).is(":visible");
-    },
+      if (!stringRegex.test(from_name)) {
+        nameError = "Name cannot include a number!";
+      }
+      if (emailError || nameError) {
+        alert(`${nameError || ""} \n${emailError || ""}`);
+      } else {
+        $this = $("#sendMessageButton");
+        $this.prop("disabled", true);
+        $("#loading-img").html(
+          '<img alt="" src="img/loader.gif" style="width: 20px" />'
+        );
+        emailjs
+          .send("service_gmail", "template_default", {
+            from_name,
+            subject,
+            reply_to,
+            message,
+            to_name: "Fatan",
+          })
+          .then(
+            function () {
+              alert("Your message has been sent! ");
+              $("#contactForm").trigger("reset");
+            },
+            function (error) {
+              alert(
+                `Sorry ${
+                  from_name.split()[0]
+                }, it seems that my mail server is not responding. Please try again later!`
+              );
+            }
+          )
+          .finally(function () {
+            $("#loading-img").html("");
+            setTimeout(function () {
+              $this.prop("disabled", false);
+            }, 1000);
+          });
+      }
+    }
   });
-
-  $('a[data-toggle="tab"]').click(function (e) {
-    e.preventDefault();
-    $(this).tab("show");
-  });
-});
-
-/*When clicking on Full hide fail/success boxes */
-$("#name").focus(function () {
-  $("#success").html("");
-});
+})();
